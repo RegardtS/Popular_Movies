e78b6533e614a8a32c7c825dbe9e6250b87097d2
Regi <regardt@myself.com>
2015-06-10 21:32:21 +0200

    Work in progress, reworked a few things

---
+++ app/src/main/java/nanodegree/regi/popularmovies/Model/Company.java
@@ -0,0 +1,25 @@
+package nanodegree.regi.popularmovies.Model;
+
+/**
+ * Created by Regardt on 2015-06-10.
+ */
+public class Company {
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/nanodegree/regi/popularmovies/Model/Genre.java
@@ -0,0 +1,26 @@
+package nanodegree.regi.popularmovies.Model;
+
+/**
+ * Created by Regardt on 2015-06-10.
+ */
+public class Genre {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
